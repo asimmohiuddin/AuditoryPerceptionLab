b596ebf3c8585dd359b8af344f1f1899f4173c8a
asimmohiuddin <asimmohiuddin@users.noreply.github.com>
2016-03-08 17:13:43 -0500

    Update ReadBuckeye.java

--- ReadBuckeye.java
+++ ReadBuckeye.java
@@ -4,10 +4,10 @@ public class ReadBuckeye{
   public static main void (String args[]) throws FileNotFoundException{
     
     Scanner s = new Scanner(new File("BuckeyeWords.txt"));
-    double timeLowest = 50000, timeHighest = 0;
+    double timeLowest = 50000, previousEnd = 0, previousTimeLowest = 0;
     boolean isConsecutive = true, isNewFile = false;
-    int previousID = -1;
-    String sentence = "";
+    int previousID = -1, previousTalkerNumber = 0;
+    String sentence = "", previousFileName = "";
     while (s.hasNextLine()){
       String line = s.nextLine();
       Scanner lineScanner = new Scanner(line);
@@ -25,20 +25,23 @@ public class ReadBuckeye{
       if (index < previousID) isNewfile = true;
       else isNewfile = false;
       if (timeStart < timeLowest) timeLowest = timeStart;
-      if ((word.startsWith("<") || word.startsWith("{")) && !(word.startsWith("<HES") ||
-         word.startsWith("<CUT") || word.startsWith("<EXT") || word.startsWith("<EXCL"))) {
-         timeHighest = timeStart;
+      if (((word.startsWith("<") || word.startsWith("{")) && !(word.startsWith("<HES") ||
+         word.startsWith("<CUT") || word.startsWith("<EXT") || word.startsWith("<EXCL"))) || isNewFile) {
          if (!isConsecutive) {
-           System.out.println(fileName + "," + talkerNumber + "," + timeLowest + "," + timeHighest + "," + sentence);
+           System.out.println(previousFileName + "," + previousTalkerNumber + "," + previousTimeLowest + "," + previousEnd + "," + sentence);
          }
          timeLowest = 50000;
          isConsecutive = true;
          sentence = "";
       } else {
         isConsecutive = false;
-        previousID = index;
         sentence = sentence + " " + word;
       }
+      previousEnd = timeEnd;
+      previousID = index;
+      previousTalkerNumber = talkerNumber;
+      previousFileName = fileName;
+      previousTimeLowest = timeLowest;
     }
   }
 }
